7cfa27416df18986d0991d8eca825ab9773809c5
BRCMOURA <brcmoura@gmail.com>
2024-10-25 21:41:51 -0300

    Add files via upload

---
+++ lista03.py
@@ -0,0 +1,35 @@
+# 1) Crie uma classe chamada "Carro" com os atributos marca, ano e cor. Em seguida, crie um objeto dessa classe e imprima seus atributos.
+
+# class Carro:
+#     def __init__(self, marca, ano, cor):
+#         self.marca = marca
+#         self.ano = ano
+#         self.cor = cor
+    
+#     def informacaoSaida(self):
+#         print(f'O carro de marca', self.marca, 'ano', self.ano, 'e cor', self.cor)
+
+# carro1 = Carro(input('Marca: '), input('Ano: '), input('Cor: '))
+# carro2 = Carro(input('Marca: '), input('Ano: '), input('Cor: '))
+
+# carro1.informacaoSaida()
+# carro2.informacaoSaida()
+
+# 2) Crie uma classe "Pessoa" e o usuário insira os atributos de dados pessoais fictícios. Crie um objeto e exiba uma mensagem usando esses atributos.
+
+# class Pessoa:
+#     def __init__(self, nome, sobrenome, idade, cpf, cidade):
+#         self.nome = nome
+#         self.sobrenome = sobrenome
+#         self.idade = idade
+#         self.cpf = cpf
+#         self.cidade = cidade
+
+#     def informacaoSaida(self):
+#         print(f'O nome do usuário é:', self.nome, self.sobrenome,',sua idade é', self.idade,'anos, CPF:', self.cpf, ',cidade:', self.cidade)
+
+# pessoa1 = Pessoa(input('Nome: '), input('Sobrenome: '), input('Idade: '), input('CPF: '), input('Cidade: '))
+# pessoa2 = Pessoa(input('Nome: '), input('Sobrenome: '), input('Idade: '), input('CPF: '), input('Cidade: '))
+
+# pessoa1.informacaoSaida()
+# pessoa2.informacaoSaida()
